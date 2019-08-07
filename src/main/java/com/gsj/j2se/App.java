7750cd0fea3269e1a3cc994c7c2cdf69e0555024
GO <GO@192.168.2.3>
2019-08-07 09:39:13 +0800

    test

--- src/main/java/com/gsj/j2se/App.java
+++ src/main/java/com/gsj/j2se/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args ) throws ClassNotFoundException
     {
     	 Class.forName("com.mysql.jdbc.Driver");
-         System.out.println("初始化驱动成功");
+         System.out.println("加载驱动成功");
     }
 }
